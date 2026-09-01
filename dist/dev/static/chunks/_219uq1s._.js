e5b67177d2d8626936aa1f1bc046b1cc4c15e972
Priyanshu <Priyanshuramchandani41@gmail.com>
2026-09-01 14:57:32 +0530

    fix cloudflare opennext build configuration

--- dist/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0h-_365._.css","static/chunks/_0900_em._.js","static/chunks/node_modules_1ukv13p._.js","static/chunks/src_1w86rlp._.js"],
-    source: "entry"
-});
--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   reactCompiler: true,
-  distDir: "dist",
   images: {
     unoptimized: true,
   },
 };
 
 export default nextConfig;
-import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
+
+import("@opennextjs/cloudflare").then(
+  (m) => m.initOpenNextCloudflareForDev()
+);
